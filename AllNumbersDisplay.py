6620fd070d50f7c6002b57781b478ed52deff094
Neotic Store <pijuspuslapis@gmail.com>
2021-06-15 14:43:08 +0300

    All Numbers Display
    This Simple Python Script Prints All Numbers By Adding 1 To The Current Number.

---
+++ AllNumbersDisplay.py
@@ -0,0 +1,10 @@
+import time
+
+print("This Simple Python Script Prints All Numbers By Adding 1 To The Current Number.")
+time.sleep(5)
+
+n = 1
+while True:
+    print(n)
+    n = n + 1
+    time.sleep(0.5)
